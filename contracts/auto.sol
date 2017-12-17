8490fa975e4549291951a0736e1543a989a69560
souradeep-das <dsouradeep2@gmail.com>
2017-12-18 01:18:42 +0530

    Add files via upload

--- contracts/auto.sol
+++ contracts/auto.sol
@@ -58,19 +58,52 @@ contract SomeContract {
 
   }
 
-  uint[] public parts_quantity;
 
   mapping (bytes32 => uint) parts_mapping;
+  mapping (bytes32 => uint) id_mapping;
+  mapping (bytes32 => uint) time_mapping;
+  mapping (bytes32 => bytes32) hashing_item;
 
-  function addparts(bytes32 name, uint quantity) only_OEM {
+  function addparts(bytes32 name, uint quantity, uint ids) only_OEM {
 
-    parts_mapping[name]=quantity;
+    uint p=parts_mapping[name];
+    parts_mapping[name]=p+quantity;
+    uint time_now=now;
+    id_mapping[name] = ids;
+    time_mapping[name]=time_now;
+    hashing_item[name]=sha3(name,ids,time_now);
 
   }
 
   function display(bytes32 part_name) constant returns(uint) {
 
     return parts_mapping[part_name];
+
+  }
+
+  function display_time(bytes32 part_name) constant returns(uint) {
+
+    return time_mapping[part_name];
+
+  }
+
+  function use_OEM_Parts(bytes32 name_of_part, uint how_many) returns (uint) {
+
+    parts_mapping[name_of_part] = parts_mapping[name_of_part] - how_many;
+    return parts_mapping[name_of_part];
+
+  }
+  function auth_part(bytes32 name, uint id_item) constant returns(uint) {
+
+    uint  time_created=time_mapping[name];
+    bytes32 hash_temp=sha3(name,id_item,time_created);
+    bytes32 orig_hash=hashing_item[name];
+    if(hash_temp==orig_hash) {
+      return 1;
+    }
+    else {
+      return 2;
+    }
 
   }
 
