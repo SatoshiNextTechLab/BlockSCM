4a7dd89d611c55a875835cc3f43d4d1bcc493ad7
souradeep-das <dsouradeep2@gmail.com>
2017-12-17 23:51:03 +0530

    Add files via upload

--- contracts/auto.sol
+++ contracts/auto.sol
@@ -7,14 +7,6 @@
 //they are also recorded in the blockchain while
 //each part is installed in a vehicle and the
 //information of vehicle is recorded in the ledger as well.
-function jo oem call karenge :
-1) created parts
-2  send parts to manufacturer(add to sent array)
-3
-function jo manufacturer call karega :
-1 pop from oem sent array
-2 check for defects
-3
 
 
 pragma solidity ^0.4.0;
@@ -79,22 +71,7 @@ contract SomeContract {
   function display(bytes32 part_name) constant returns(uint[]) {
 
     return parts_mapping[part_name].number;
-  }
 
   }
 
-yeah man nice work !!!!!!!!!!!!!!!!!!
-cool terko kuch khana ha to khale bata de kitni der me vapas start karna hai??????
-11 seems perfect i am going byeee!!!!!!!!!!!!!! if you wanna talk text me
-yeah!!
-now listen!
-i will add that if already item exists then ill update the number
-its working fine.
-can you do one thing.the things i add can you put in array! if its required
-its working i just need to add the situation where overwrite happens ill just update those
-yeah!!!<------------
-do u want to take a break??
-i am gonna eat now so..ok bro
-half an hour or 1 hour
-tu bata
-cool!!! bye! bye! and copy the code just in case .. thanks
+}
