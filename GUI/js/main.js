bbde0dc6c9bbdde51d0253102bffcf964e7ba471
Projjal Gupta <paltrickontpb@gmail.com>
2017-12-18 10:26:24 +0530

    GUI update

---
+++ GUI/js/main.js
@@ -0,0 +1,8 @@
+function init() {
+  $(".hed").addClass("animated fadeInDown");
+  $(".cont1").addClass("animated fadeInDown");
+  $(".cont2").addClass("animated fadeInDown");
+  $(".cont3").addClass("animated fadeInDown");
+}
+
+$(document).ready(init());
